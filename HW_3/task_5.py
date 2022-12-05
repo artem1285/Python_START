fa30de46493cadc1331d2a0561551776ed9ff696
Artem Alekseevgit <artem_1285@mail.ru>
2022-12-05 22:48:30 +0500

    решил

--- HW_3/task_5.py
+++ HW_3/task_5.py
@@ -10,3 +10,21 @@ https://ru.wikipedia.org/wiki/Негафибоначчи
 8
 [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 """
+num = int(input('Введите число: '))
+
+
+def fib(num):
+    fib_num = []
+    a, b = 1, 1
+    for i in range(num):
+        fib_num.append(a)
+        a, b = b, a + b
+    a, b = 0, 1
+    for i in range(num+1):
+        fib_num.insert(0, a)
+        a, b = b, a - b
+    return fib_num
+
+
+fib_num = fib(num)
+print(fib(num))
