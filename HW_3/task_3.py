28f4757986a0f716b1b6234d7d58b5bc743803e4
Artem Alekseevgit <artem_1285@mail.ru>
2022-12-05 22:38:32 +0500

    решил

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -9,3 +9,11 @@
 [1.1, 1.2, 3.1, 5, 10.01]
 2.0
 """
+from random import uniform  # uniform() возвращает случайный вещественное число
+number = int(input('Введите число '))
+# round округляtn число с плавающей точкой до той цифры, которую задает пользователь
+first_lst = [round(uniform(0, 10), 2) for _ in range(number)]
+print(first_lst)
+second_lst = [round(first_lst[i] - int(first_lst[i]), 2)
+              for i in range(number)]
+print(max(second_lst) - min(second_lst))
