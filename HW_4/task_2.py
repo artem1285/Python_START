fcbe7388bfc8ce248213aeeaa3487ea8a4e7c29e
Artem Alekseevgit <artem_1285@mail.ru>
2022-12-08 14:16:38 +0500

    корректировка

--- HW_4/task_2.py
+++ HW_4/task_2.py
@@ -10,12 +10,13 @@
 """
 from random import randint
 
-n = int(input("Введите число: "))
-my_list = [randint(0, n) for i in range(n)]
+# n = int(input("Введите число: "))
+my_list = [1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9]
 print(my_list)
 result_list = []
 
 for number in my_list:
-    if number not in result_list:
+    if not my_list.count(number) > 1:
         result_list.append(number)
+
 print(result_list)
