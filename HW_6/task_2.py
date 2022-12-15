89c0c0fc7d6089129f89a43c095f88a0777aef92
Artem Alekseevgit <artem_1285@mail.ru>
2022-12-15 19:19:14 +0500

    решение

--- HW_6/task_2.py
+++ HW_6/task_2.py
@@ -13,3 +13,20 @@
 [1, 3, 5]
 [1, 2, 5, 3, 10]
 """
+from random import randrange  # randrange возвращает случайное целое число из указанного диапазона (start, stop, step)
+
+size_list = int(input('Введите размер списка: '))
+rand_list = [randrange(9) for i in range(size_list)]
+result_list1, result_list2, result_list3 = [], [], []
+print(rand_list)
+for i in range(size_list):
+    if rand_list.count(rand_list[i]) == 1:
+        result_list1.append(rand_list[i])
+    if rand_list.count(rand_list[i]) > 1:
+        result_list2.append(rand_list[i])
+        result_list2 = [i for n, i in enumerate(
+            result_list2) if i not in result_list2[:n]]
+result_list3 = [i for n, i in enumerate(rand_list) if i not in rand_list[:n]]
+print(result_list1)
+print(result_list2)
+print(result_list3)
